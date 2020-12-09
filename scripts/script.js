f3ec32713c3d84041ccda7e3879a548dd8ff6ba1
Anton Veselov <idrdi@yandex.ru>
2020-12-09 17:29:34 +0300

    feat: add initials card programmatically

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,50 @@ function formSubmitHandler(evt) {
 editProfileButton.addEventListener('click', showPopup);
 closePopupButton.addEventListener('click', closePopup);
 editProfileForm.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#cardTemplate').content;
+const cardsContainer = document.querySelector('.cards');
+
+function addCard(name, imageUrl) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardTitle.textContent = name;
+
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = imageUrl;
+
+  cardsContainer.prepend(cardElement);
+}
+
+function addInitialCards() {
+  initialCards.forEach(cardInfo => addCard(cardInfo.name, cardInfo.link));
+}
+
+addInitialCards();
