f5ab474d80b040e81247e39decab573aed1d52dc
Anton Veselov <idrdi@yandex.ru>
2020-12-13 15:46:40 +0300

    feat: add add-card-popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,40 @@
-let editProfileButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button');
-let popup = document.querySelector('.popup');
-let profileNameElement = document.querySelector('.profile__name');
-let profileAboutElement = document.querySelector('.profile__about');
-let usernameInput = document.querySelector('input[name="username"]');
-let aboutInput = document.querySelector('input[name="about"]');
-let editProfileForm = document.querySelector('form[name="edit-profile"]');
-
-function showPopup() {
-  usernameInput.value = profileNameElement.textContent;
-  aboutInput.value = profileAboutElement.textContent;
-  popup.classList.add('popup_opened');
-}
-
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function showPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+let editProfileButton = document.querySelector('.profile__edit-button');
+let profileNameElement = document.querySelector('.profile__name');
+let profileAboutElement = document.querySelector('.profile__about');
+
+let editProfilePopup = document.querySelector('.edit-profile-popup');
+let closeEditProfilePopupButton = editProfilePopup.querySelector('.popup__close-button');
+let usernameInput = editProfilePopup.querySelector('input[name="username"]');
+let aboutInput = editProfilePopup.querySelector('input[name="about"]');
+let editProfileForm = editProfilePopup.querySelector('form[name="edit-profile"]');
+
+function showEditProfilePopup() {
+  usernameInput.value = profileNameElement.textContent;
+  aboutInput.value = profileAboutElement.textContent;
+
+  showPopup(editProfilePopup);
+}
+
+function editProfileFormSubmitHandler(evt) {
   evt.preventDefault();
 
   profileNameElement.textContent = usernameInput.value;
   profileAboutElement.textContent = aboutInput.value;
 
-  popup.classList.remove('popup_opened');
+  closePopup(editProfilePopup);
 }
 
-editProfileButton.addEventListener('click', showPopup);
-closePopupButton.addEventListener('click', closePopup);
-editProfileForm.addEventListener('submit', formSubmitHandler);
+editProfileButton.addEventListener('click', showEditProfilePopup);
+closeEditProfilePopupButton.addEventListener('click', () => closePopup(editProfilePopup));
+editProfileForm.addEventListener('submit', editProfileFormSubmitHandler);
 
 const cardTemplate = document.querySelector('#cardTemplate').content;
 const cardsContainer = document.querySelector('.cards');
@@ -50,3 +56,26 @@ function addInitialCards() {
 }
 
 addInitialCards();
+
+let addCardButton = document.querySelector('.profile__add-button');
+
+let addCardPopup = document.querySelector('.add-card-popup');
+let closeAddCardPopupButton = addCardPopup.querySelector('.popup__close-button');
+let cardNameInput = addCardPopup.querySelector('input[name="name"]');
+let cardLinkInput = addCardPopup.querySelector('input[name="link"]');
+let addCardForm = addCardPopup.querySelector('form[name="add-card"]');
+
+function addCardFormSubmitHandler(evt) {
+  evt.preventDefault();
+
+  addCard(cardNameInput.value, cardLinkInput.value);
+
+  cardNameInput.value = '';
+  cardLinkInput.value = '';
+
+  closePopup(addCardPopup);
+}
+
+addCardButton.addEventListener('click', () => showPopup(addCardPopup));
+closeAddCardPopupButton.addEventListener('click', () => closePopup(addCardPopup));
+addCardForm.addEventListener('submit', addCardFormSubmitHandler);
